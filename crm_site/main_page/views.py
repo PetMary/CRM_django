7953b68e0457cb8d291b2c8b1aab3a82c87753de
PetMary <m_petrova01@mail.ru>
2023-05-26 21:55:30 +0300

    page orders fixed

--- crm_site/main_page/views.py
+++ crm_site/main_page/views.py
@@ -1,9 +1,12 @@
 import os
+from datetime import datetime as date_t
 import datetime
+
 import dateutil.relativedelta
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.core.serializers import serialize
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -12,29 +15,25 @@ import json
 
 from .models import Services, Order, Customer
 
-
 def ex(request):
     # data = Order.objects.filter(user=request.user.id)
-    data = Services.objects.filter(user=request.user.id)  # поменять на определение по name
-    return render(request, 'main_page/index.html', {'data': data})
-    # добавить для второго графика
-    # chart_2 = {}
-    # for d in data:
-    #     try:
-    #         chart_2[d.id_services.name] += 1
-    #     except KeyError:
-    #         chart_2[d.id_services.name] = 1
-    # new_chart_2 = list(map(list, chart_2.items()))
-    #
-    # try:
-    #     with open("media/user_{0}/data_file.json".format(request.user.id), "w") as write_file:
-    #         json.dump(new_chart_2, write_file)
-    # except FileNotFoundError:
-    #     os.mkdir("media/user_{0}".format(request.user.id))
-    #     with open("media/user_{0}/data_file.json".format(request.user.id), "w") as write_file:
-    #         json.dump(new_chart_2, write_file)
+    data1 = Order.objects.filter(user=request.user.id).order_by('-date_order')
+    data = Order.objects.filter(user=request.user.id).order_by('-date_order').values('id', 'id_customer__name',
+                                                                                    'id_services__name',
+                                                                                    'date_order__date',
+                                                                                    'date_order__time',
+                                                                                    'id_services__price',)
 
-    # return render(request, "main_page/index.html", {'data': data})
+    def json_date_handler(obj):
+        if hasattr(obj, 'isoformat'):
+            return obj.isoformat()
+        else:
+            raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
+    json_data = json.dumps(list(data), default=json_date_handler)
+
+    name_ser = Services.objects.filter(user=request.user.id)
+    return render(request, 'main_page/index.html', {'data': data1, 'json_data': json_data, 'name_ser': name_ser})
 
 
 def home(request):
@@ -67,8 +66,8 @@ def crm_information(request):
         chart_1[date_chart.strftime("%B")] = 0
         date_chart += dateutil.relativedelta.relativedelta(months=1)
 
-    date_chart_1 = datetime.datetime.combine(date_chart_1, datetime.datetime.min.time())
-    date_chart = datetime.datetime.combine(date_chart, datetime.datetime.min.time())
+    date_chart_1 = date_t.combine(date_chart_1, date_t.min.time())
+    date_chart = date_t.combine(date_chart, date_t.min.time())
     for d in data:
         if d.date_order.timestamp() >= date_chart_1.timestamp() and d.date_order.timestamp() < date_chart.timestamp():
             chart_1[d.date_order.strftime("%B")] += 1
@@ -96,9 +95,23 @@ def crm_information(request):
 
 @login_required(login_url='login')
 def crm_orders(request):
-    data = Order.objects.filter(user=request.user.id).order_by('date_order')
+    data1 = Order.objects.filter(user=request.user.id).order_by('-date_order')
+    data = Order.objects.filter(user=request.user.id).order_by('-date_order').values('id', 'id_customer__name',
+                                                                                     'id_services__name',
+                                                                                     'date_order__date',
+                                                                                     'date_order__time',
+                                                                                     'id_services__price', )
+
+    def json_date_handler(obj):
+        if hasattr(obj, 'isoformat'):
+            return obj.isoformat()
+        else:
+            raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
+
+    json_data = json.dumps(list(data), default=json_date_handler)
+
     name_ser = Services.objects.filter(user=request.user.id)
-    return render(request, 'main_page/orders.html', {'data': data, 'name_ser': name_ser })
+    return render(request, 'main_page/orders.html', {'data': data1, 'json_data': json_data, 'name_ser': name_ser})
 
 
 @login_required(login_url='login')
